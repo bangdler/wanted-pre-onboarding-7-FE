4b94b99cc185c1a2bba75de4b3a979ea4a6ee685
bangdler <zbthz90@gmail.com>
2023-07-07 00:26:20 +0900

    refactor: App, Nav 중첩 라우팅 반영

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,60 @@ import './App.css';
 import Router from './Router';
 import Route from './Route';
 import Nav from './Nav';
+import Routes from './Routes';
 
 function App() {
   return (
     <Router>
-      <Route
-        path={'/'}
-        component={
-          <div>
-            <Nav />
-            home
-          </div>
-        }
-      />
-      <Route
-        path={'/about'}
-        component={
-          <div>
-            <Nav />
-            about
-          </div>
-        }
-      />
-      <Route
-        path={'/user'}
-        component={
-          <div>
-            {' '}
-            <Nav />
-            user
-          </div>
-        }
-      />
+      <Routes>
+        <Route
+          path={'/'}
+          component={
+            <div>
+              <Nav />
+              home
+            </div>
+          }
+        >
+          <Route
+            path={'/2'}
+            component={
+              <div>
+                <Nav />
+                home2
+              </div>
+            }
+          />
+        </Route>
+        <Route
+          path={'/about'}
+          component={
+            <div>
+              <Nav />
+              about
+            </div>
+          }
+        >
+          <Route
+            path={'/2'}
+            component={
+              <div>
+                <Nav />
+                about2
+              </div>
+            }
+          />
+        </Route>
+        <Route
+          path={'/user'}
+          component={
+            <div>
+              <Nav />
+              user
+            </div>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -15,10 +15,26 @@ const Nav = () => {
       <div
         style={{ margin: '20px', pointer: 'cursor' }}
         onClick={() => {
+          push('/2');
+        }}
+      >
+        홈2로 이동하기
+      </div>
+      <div
+        style={{ margin: '20px', pointer: 'cursor' }}
+        onClick={() => {
           push('/about');
         }}
       >
         about 페이지로 이동하기
+      </div>
+      <div
+        style={{ margin: '20px', pointer: 'cursor' }}
+        onClick={() => {
+          push('/about/2');
+        }}
+      >
+        about 2로 이동하기
       </div>
       <div
         style={{ margin: '20px', pointer: 'cursor' }}
