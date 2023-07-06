266106c8f9db68e3f9f65bf393e83caff1789dbd
bangdler <zbthz90@gmail.com>
2023-07-07 00:25:25 +0900

    feat: 중첩된 Route 처리 로직 구현

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,22 +1,55 @@
-import { ReactElement } from 'react';
+import React, {
+  Children,
+  ReactElement,
+  ReactNode,
+} from 'react';
 import useRouter from './useRouter';
 
 const Route = ({
   path,
   component,
+  children,
+  parentPath,
 }: {
   path: string;
   component: ReactElement;
+  children: ReactNode;
+  parentPath?: string;
 }) => {
   const { path: routerPath, addRoute } = useRouter();
 
   addRoute(path);
 
-  if (path !== routerPath) {
-    return null;
+  if (path === routerPath) {
+    return component;
   }
 
-  return component;
+  // nested Route 인 경우 parentPath 가 있으므로 비교
+  if (parentPath) {
+    const newPath = parentPath === '/' ? path : parentPath + path;
+    if (newPath === routerPath) {
+      return component;
+    }
+  }
+
+  // 하위 컴포넌트로 Route 가 있다면 Route 를 return
+  if (
+    Children.count(children) === 1 &&
+    children.props.path &&
+    children.props.component
+  ) {
+    // route 인 경우 routerPath 가 path 를 포함한다면
+    const regex = new RegExp(path);
+    if (regex.test(routerPath)) {
+      return React.cloneElement(
+        children,
+        { parentPath: path, ...children.props },
+        children.children,
+      );
+    }
+  }
+
+  return null;
 };
 
 export default Route;
